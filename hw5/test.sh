2c8e04a61227f04b72fe7dbe313097aaee32aa4f
Xueyang Liu <xueyang.liu@qq.com>
2021-11-24 22:30:51 -0500

    add 1 thread pt test

--- hw5/test.sh
+++ hw5/test.sh
@@ -4,6 +4,9 @@ make
 echo "**********Sequential**********"
 ./rainfall_seq 8 50 0.5 4096 measurement_4096x4096.in > my_4096_seq.out
 python3 check.py 4096 measurement_4096x4096.out my_4096_seq.out
+echo "**********1 Thread**********"
+./rainfall_pt 1 50 0.5 4096 measurement_4096x4096.in > my_4096_1thread.out
+python3 check.py 4096 measurement_4096x4096.out my_4096_1thread.out
 echo "**********2 Thread**********"
 ./rainfall_pt 2 50 0.5 4096 measurement_4096x4096.in > my_4096_2thread.out
 python3 check.py 4096 measurement_4096x4096.out my_4096_2thread.out
